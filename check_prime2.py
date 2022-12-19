234f9c0c7fe2ea358541c302a2a237ad4c9bf595
rajesh-yadav-tfs <107191719+rajesh-yadav-tfs@users.noreply.github.com>
2022-12-19 16:21:23 +0530

    Update check_prime2.py

--- check_prime2.py
+++ check_prime2.py
@@ -1,20 +1,17 @@
 def check_prime(n):
-    for i in range(2,int(n**0.5)+1):
+    for i in range(2,n):
         if n%i==0:
             return 1 #non prime
     return 0 #prime
-    
+
+# flag = check_prime(n)
 while True:
-    n=int(input("please enter a number: "))
+    n=int(input("Please enter a number: "))
     flag = check_prime(n)
-    if flag==1:
-        print(f"{n} is a non prime number")
+    if flag==1 or n==0 or n==1:
+        print(f"{n} is a not a prime number")
+        continue
     else:
         print(f"{n} is a prime number")
-        a=input("do you want to continue:(y/n): ")
-        if a.lower()=='y':
-            print("ok continue")
-        else:
-            print("ok exiting, Thankyou")
-            break
+        break
     
